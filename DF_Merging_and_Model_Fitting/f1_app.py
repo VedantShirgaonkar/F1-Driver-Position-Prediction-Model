c64b43606c0dc933b0742c169b43de58231cc6e6
VedantShirgaonkar <vedxntshirgaonkar@gmail.com>
2024-10-09 10:47:07 +0530

    Adding new requirements

--- DF_Merging_and_Model_Fitting/f1_app.py
+++ DF_Merging_and_Model_Fitting/f1_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 
 # Load the trained model pipeline
-pipeline = joblib.load('f1_pipeline.pkl')
+pipeline = joblib.load('/Users/rahul/Desktop/F1 Project/DF_Merging_and_Model_Fitting/f1_pipeline.pkl')
 
 # Streamlit App Interface
 st.title("F1 Driver Position Prediction")
